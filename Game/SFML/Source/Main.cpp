90d98b6bddfa299326c3bca7a7915322f843c99d
zhangping <zhang1991426312@163.com>
2020-10-07 20:47:22 +0800

    Add test code and build task

---
+++ Game/SFML/Source/Main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(int args, char* argv[])
+{
+    std::cout << "Hello, World!" << std::endl;
+    int i = 0;
+    i += 5;
+    std::cout << "i: " << i << std::endl;
+
+    return 0;
+}
