7be69d06e4d9b5145e592ba2580df806c14dcfae
张平 <zhangping@zhangpingdeMac-Pro.local>
2020-10-09 18:49:15 +0800

    SFML support on Xcode

--- Game/SFML/premake5.lua
+++ Game/SFML/premake5.lua
@@ -34,7 +34,7 @@ project "SFML"
         -- Copy dlls
         prebuildcommands 
         {
-            ("{COPY} \"%{wks.location}../Third/SFML-2.5.1/bin/%{cfg.system}/%{cfg.architecture}/*.dll\" \"%{cfg.buildtarget.directory}\""),
+            ("{COPY} \"%{wks.location}../Third/SFML-2.5.1/bin/%{cfg.system}/%{cfg.architecture}/*.dll\" \"%{cfg.buildtarget.directory}\"")
         }
 
     filter { "system:windows", "configurations:Debug" }
@@ -45,5 +45,28 @@ project "SFML"
 
     -- Mac configuration
     filter "system:macosx"
-    systemversion "10.14"   -- 我的Mac版本是10.14.5，低于development target的10.15， 为了避免手动修改，改成10.14
+        systemversion "10.14"   -- 我的Mac版本是10.14.5，低于development target的10.15， 为了避免手动修改，改成10.14
         includedirs     { g_WorkspaceFolder .. "/%{prj.name}/Source/" }-- Xcode: User Header Search Paths
+        sysincludedirs  { "%{IncludeDirs.SFML}" }--Xcode: Header Search Paths
+        links           { "sfml-system.framework", "sfml-window.framework", "sfml-graphics.framework", "freetype.framework" }
+        linkoptions 
+        { 
+            "-F" .. g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks",
+            "-Xlinker -rpath -Xlinker " .. g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks"
+        }
+
+        --syslibdirs      { g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks" }
+        --libdirs      { g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks" }
+        -- runpathdirs     
+        -- { 
+        --     g_WorkspaceFolder .. "/../bin/%{prj.name}",
+        --     g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks",
+        --     g_WorkspaceFolder .. "/../Third/SFML-2.5.1/lib/%{cfg.system}/extlibs"
+        -- }
+        -- prebuildcommands
+        -- {
+        --     ("{COPY} \"%{wks.location}/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks/sfml-system.framework\" \"%{cfg.buildtarget.directory}\""),
+        --     ("{COPY} \"%{wks.location}/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks/sfml-window.framework\" \"%{cfg.buildtarget.directory}\""),
+        --     ("{COPY} \"%{wks.location}/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks/sfml-graphics.framework\" \"%{cfg.buildtarget.directory}\""),
+        --     ("{COPY} \"%{wks.location}/../Third/SFML-2.5.1/lib/%{cfg.system}/Frameworks/freetype.framework\" \"%{cfg.buildtarget.directory}\"")
+        -- }
